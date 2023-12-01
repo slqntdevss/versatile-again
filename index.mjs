bf5c9786244c85daaa70033b7a40f62f2e682d6c
slqnt <94934670+slqntdevss@users.noreply.github.com>
2023-12-01 23:28:51 +0000

    quick fix

--- index.mjs
+++ index.mjs
@@ -1,11 +1,14 @@
 import createServer from "@tomphttp/bare-server-node";
 import http from "http";
-import nodeStatic from "node-static";
-
+const express = require('express');
+const serve   = require('express-static');
 const bare = createServer("/bare/");
-const serve = new nodeStatic.Server("static/");
 
 const server = http.createServer();
+
+const app = express();
+ 
+app.use(serve(__dirname + '/static'));
 
 server.on("request", (req, res) => {
   if (bare.shouldRoute(req)) {
