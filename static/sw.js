01642d2334c1fb71665e07fe95812ead5b90ca31
slqnt <94934670+slqntdevss@users.noreply.github.com>
2023-12-02 00:22:23 +0000

    forgot shut

--- static/sw.js
+++ static/sw.js
@@ -1,9 +1,10 @@
-importScripts('./uv/uv.sw.js');
+importScripts("./uv/uv.bundle.js");
+importScripts("./uv/uv.config.js");
+importScripts("./uv/uv.sw.js");
 
-const sw = new UVServiceWorker();
+const UV = new UVServiceWorker();
 
-self.addEventListener('fetch', event =>
-    event.respondWith(
-        sw.fetch(event)
-    )
-);
+self.addEventListener("fetch", (event) => {
+  if (event.request.url.startsWith(location.origin + "/uv/service/"))
+    event.respondWith(UV.fetch(event));
+});
